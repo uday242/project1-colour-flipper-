de062b696947d08a4319db9660f8af6c486362d3
uday242 <144389958+uday242@users.noreply.github.com>
2024-01-11 08:38:57 -0800

    Add files via upload

---
+++ setup/hex.js
@@ -0,0 +1,19 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+
+
+const colors = ["orange", "black", "purple"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  // console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
